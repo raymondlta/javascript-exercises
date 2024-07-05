3d79bcbf8c1dc24358458d1526ccf5c813a6faf3
raymond <taraymondl1@gmail.com>
2024-07-05 16:50:59 -0700

    Completed exercies 10 to 12
    Finished all the exercies from this section and passed all the tests.
    Solution produced not as clean as provided solution, but generally
    appears to be on track.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const newNum = parseInt(num);
 
+    if (newNum < 0) {
+        return "OOPS";
+    }
+    else if (newNum === 0) {
+        return 0;
+    }
+    else if (newNum === 1) {
+        return 1;
+    }
+
+    let first_val = 0;
+    let second_val = 1;
+    let factorial = 1;
+
+    for (let i = 1; i < newNum; i++) {
+        factorial = first_val + second_val;
+        first_val = second_val;
+        second_val = factorial;
+    }
+
+    return factorial;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((person1, person2) => {
+        let person1Age;
+        let person2Age;
+        if (!("yearOfDeath" in person1)) {
+            person1Age = 2024 - person1.yearOfBirth;
+        }
+        if (!("yearOfDeath" in person2)) {
+            person2Age = 2024 - person2.yearOfBirth;
+        }
+        person1Age = person1Age ? person1Age : person1.yearOfDeath - person1.yearOfBirth;
+        person2Age = person2Age ? person2Age : person2.yearOfDeath - person2.yearOfBirth;
 
+        return person1Age > person2Age ? -1 : 1;
+    });
+
+    return people[0];
 };
 
 // Do not edit below this line
