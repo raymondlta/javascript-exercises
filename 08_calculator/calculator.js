d4cc32d231a4ad6868ea0f2d1c7d701a0b10f7a8
raymond <taraymondl1@gmail.com>
2024-07-05 15:39:44 -0700

    Completed exercise 8 and 9
    Exercise 8 completed with similar solutions to provided. Exercise 9
    completed using regex instead of creating string literal of all alpha
    numerics. Additionally, provided solution appeared cleaner by comparing
    string to reversed string instead of looping and checking individual
    chars

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, nextVal) => sum + nextVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, nextVal) => sum * nextVal, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+
+  for (let i = 1; i < num;) {
+    total *= ++i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const str_arr = str.toLowerCase().split('');
+    let first_index = 0;
+    let last_index = str_arr.length - 1;
 
+    if (last_index <= 0) {
+        return false;
+    }
+
+    for (;first_index < last_index;) {
+        if (!/[a-z0-9]/i.test(str_arr[first_index])) {
+            first_index++;
+        }
+        else if (!/[a-z0-9]/i.test(str_arr[last_index])) {
+            last_index--;
+        }
+        else if (str_arr[first_index] != str_arr[last_index]) {
+            return false;
+        }
+        else {
+            first_index++;
+            last_index--;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
